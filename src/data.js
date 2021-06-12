9f3ce390a4256eb0e293aa2002e47a945302d525
protee <inezaprotogene@gmail.com>
2021-06-12 23:20:35 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export default [
+	{ id: '1', imageName: 'banner1.jpg', tag: 'free' },
+	{ id: '2', imageName: 'banner2.jpg', tag: 'new' },
+	{ id: '3', imageName: 'banner3.jpg', tag: 'pro' },
+	{ id: '4', imageName: 'banner4.jpg', tag: 'pro' },
+	{ id: '5', imageName: 'banner5.jpg', tag: 'free' }
+
+];
